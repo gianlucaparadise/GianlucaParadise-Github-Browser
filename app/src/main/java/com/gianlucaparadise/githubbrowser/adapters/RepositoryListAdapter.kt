6e72cfe52f9f78c5753ccfe0e80a309c76202b80
gianlucaparadise <gianlucaparadise@gmail.com>
2020-05-24 10:30:26 +0200

    Added RepositoryList view in HomePage - DataBinding setup - Created RepositoryListAdapter

---
+++ app/src/main/java/com/gianlucaparadise/githubbrowser/adapters/RepositoryListAdapter.kt
@@ -0,0 +1,59 @@
+package com.gianlucaparadise.githubbrowser.adapters
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.gianlucaparadise.githubbrowser.R
+import com.gianlucaparadise.githubbrowser.data.Repository
+import com.gianlucaparadise.githubbrowser.databinding.RepositoryListItemBinding
+
+class RepositoryListAdapter :
+    ListAdapter<Repository, RepositoryListAdapter.ViewHolder>(RepositoryDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.repository_list_item,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: RepositoryListItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentRepository: Repository) {
+            with(binding) {
+                repository = currentRepository
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+private class RepositoryDiffCallback : DiffUtil.ItemCallback<Repository>() {
+
+    override fun areItemsTheSame(
+        oldItem: Repository,
+        newItem: Repository
+    ): Boolean {
+        return oldItem.name == newItem.name && oldItem.owner?.login == newItem.owner?.login
+    }
+
+    override fun areContentsTheSame(
+        oldItem: Repository,
+        newItem: Repository
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
