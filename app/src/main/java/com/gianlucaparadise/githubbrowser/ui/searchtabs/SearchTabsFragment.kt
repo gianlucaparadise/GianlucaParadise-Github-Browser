c1b3c34c7804da3ddf0102a713839e8d0e586de9
gianlucaparadise <gianlucaparadise@gmail.com>
2020-05-25 21:55:43 +0200

    Search query handling with debounce

--- app/src/main/java/com/gianlucaparadise/githubbrowser/ui/searchtabs/SearchTabsFragment.kt
+++ app/src/main/java/com/gianlucaparadise/githubbrowser/ui/searchtabs/SearchTabsFragment.kt
@@ -40,7 +40,7 @@ class SearchTabsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SearchTabsViewModel::class.java)
-        // TODO: Use the ViewModel
+        binding.viewmodel = viewModel
     }
 
 }
